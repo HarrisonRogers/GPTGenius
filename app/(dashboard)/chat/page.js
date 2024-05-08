997f60afbe372e73493c9fdeb17b3950e01c9c8c
HarrisonRogers <harrison.rogers05@gmail.com>
2024-05-09 09:14:56 +1200

    app layout

---
+++ app/(dashboard)/chat/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ChatPage = () => {
+  return <div className="text-7xl">ChatPage</div>
+}
+
+export default ChatPage
---
+++ app/(dashboard)/tours/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ToursPage = () => {
+  return <div className="text-7xl">ToursPage</div>
+}
+
+export default ToursPage
