b662e2d4735c67219a3a73b672cc678e44b25032
HarrisonRogers <harrison.rogers05@gmail.com>
2024-05-13 11:12:48 +1200

    add working chat bot

--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,24 @@
 'use server'
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
 
 export const generateChatResponse = async (chatMessage) => {
-  console.log(chatMessage)
-  return 'awesome'
+  try {
+    const res = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: 'you are a helpful assistant' },
+        ...chatMessage,
+      ],
+      model: 'gpt-3.5-turbo',
+      temperature: 0,
+    })
+    console.log(res.choices[0].message)
+    console.log(res)
+    return res.choices[0].message
+  } catch (error) {
+    return null
+  }
 }
